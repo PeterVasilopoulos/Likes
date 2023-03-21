da9359234d9058a832e9c74de1e979848675c89f
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-03-21 15:54:47 -0400

    Assignment completed

---
+++ script.js
@@ -0,0 +1,23 @@
+// Increase Like Counter 1
+var likeCounter1 = 9;
+function increaseCounter1() {
+    var counter = document.querySelector("#post1-likes");
+    likeCounter1++;
+    counter.innerText = likeCounter1;
+}
+
+// Increase Like Counter 2
+var likeCounter2 = 12;
+function increaseCounter2() {
+    var counter = document.querySelector("#post2-likes");
+    likeCounter2++;
+    counter.innerText = likeCounter2;
+}
+
+// Increase Like Counter 3
+var likeCounter3 = 9;
+function increaseCounter3() {
+    var counter = document.querySelector("#post3-likes");
+    likeCounter3++;
+    counter.innerText = likeCounter3;
+}
